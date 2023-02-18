411b1a8289f0aaa8d8b4dd7c1fd2611948b1cedd
idksmg <guz22097@uvg.edu.gt>
2023-02-18 17:33:43 +0100

    Version 1.2

--- ILista.java
+++ ILista.java
@@ -3,15 +3,13 @@ public interface ILista<E> {
 
    void add(E element);
    void add(int index, E element);
-   void remove(int index);
+   E remove(int index);
    E get(int index);
    void set(int index, E element);
    int size();
    boolean isEmpty();
    void clear();
    int indexOf(E element);
-   int lastIndexOf(E element);
    boolean contains(E element);
-   Iterator<E> iterator();
 
 }
--- ListaDobleEncadenada.java
+++ ListaDobleEncadenada.java
@@ -1,24 +1,140 @@
 public class ListaDobleEncadenada<E> extends AbstractLista<E> {
-    protected int count;
-    protected DobleNode<E> head;
-    protected DobleNode<E> tail;
+   private Node<E> head;
+   private Node<E> tail;
+   private int size;
 
-    public ListaDobleEncadenada(){
-        head=null;
-        tail=null;
-        count=0;
-    }
-    public void addFirst(E value){
-        head = new DobleNode<E>(value, head, null);
-        if(tail == null) tail = head;
-        count++;
-    }
+   public ListaDobleEncadenada(){
+       head=null;
+       tail=null;
+       size=0;
+   }
 
-    public void addLast(E value){
-        tail = new DobleNode<E>(value, null, tail);
-        if(head==null) head = tail;
-        count++;
-    }
+   private static class Node<E>{
+       private E data;
+       private Node<E> next;
+       private Node<E> prev;
 
-    }
+       public Node(E data){
+           this.data=data;
+           this.next=null;
+           this.prev=null;
+       }
+   }
+
+   public int size(){
+       return size;
+   }
+
+   public boolean isEmpty(){
+       return size==0;
+   }
+
+   public boolean contains(E element){
+       Node<E> current = head;
+       while(current !=null){
+           if(current.data.equals(element)){
+               return true;
+           }current = current.next;
+       }
+       return false;
+   }
+
+   public void add(E element){
+       Node<E> newNode = new Node<E>(element);
+       if(head==null){
+           head=newNode;
+           tail=newNode;
+       }else{
+           tail.next=newNode;
+           newNode.prev=tail;
+           tail=newNode;
+       }
+       size++;
+   }
+   public void add(int index, E element){
+       if(index<0||index>size){
+           throw new IndexOutOfBoundsException();
+       }
+       if(index ==0){
+           Node<E> newHead = new Node<E>(element);
+           newHead.next=head;
+           if(head!=null){
+               head.prev=newHead;
+           }else {
+               tail = newHead;
+           }
+           head = newHead;
+       } else if (index == size) {
+           Node<E> newTail = new Node<E>(element);
+           newTail.prev = tail;
+           if(tail !=null){
+               tail.next=newTail;
+           }else {
+               head=newTail;
+           }
+           tail = newTail;
+       }else{
+           Node<E> current = head;
+           for(int i=0;i<index-1;i++){
+               current = current.next;
+           }
+           Node<E> newNode= new Node<E>(element);
+           newNode.next=current.next;
+           newNode.prev=current;
+           current.next.prev=newNode;
+           current.next=newNode;
+       }
+       size++;
+   }
+   public E get(int index){
+       if(index<0||index>=size){
+           throw new IndexOutOfBoundsException();
+       }
+       Node<E> current = head;
+       for(int i=0; i<index;i++){
+           current=current.next;
+       }
+       return current.data;
+   }
+
+   public E remove(int index){
+       if(index <0||index>=size){
+           throw new IndexOutOfBoundsException();
+       }
+       E removeData;
+       if(size==1){
+           removeData=head.data;
+           head=null;
+           tail=null;
+       } else if (index==0) {
+           removeData= head.data;
+           head=head.next;
+           head.prev=null;
+       } else if (index==size-1) {
+           removeData=tail.data;
+           tail=tail.prev;
+           tail.next=null;
+       }else{
+           Node<E> current = head;
+           for(int i=0; i<index; i++){
+               current=current.next;
+           }
+           removeData= current.data;
+           current.prev.next=current.next;
+           current.next.prev=current.prev;
+       }return removeData;
+   }
+   public void set(int index, E element){
+
+   }
+
+   public void clear(){
+       head=null;
+       tail=null;
+       size=0;
+   }
+
+   public int indexOf(E element){
+    return -1;
+   }
 }
